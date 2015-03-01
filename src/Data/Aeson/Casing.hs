d908a70019b4201a9910eb151643bfd7713b30c6
AndrewRademacher <andrewrademacher@gmail.com>
2015-03-01 15:20:52 -0600

    Added docs.

--- src/Data/Aeson/Casing.hs
+++ src/Data/Aeson/Casing.hs
@@ -1,3 +1,24 @@
+-- | The casing utilities allow you to specify how Aeson renders and parses
+-- the field names in JSON messages. Snake, Camel, and Pascal case are all
+-- supported. To include casing modifiers in Aeson, attach options to instances
+-- of ToJSON and FromJSON.
+--
+-- > data Person = Person
+-- >        { personFirstName :: Text
+-- >        , personLastName  :: Text
+-- >        } deriving (Generic)
+-- >
+-- > instance ToJSON Person where
+-- >    toJSON = genericToJSON $ aesonPrefix snakeCase
+-- > instance FromJSON Person where
+-- >    parseJSON = genericParseJSON $ aesonPrefix snakeCase
+--
+-- The above code will produce JSON messages like the following...
+--
+-- > {
+-- >    "first_name": "John",
+-- >    "last_name": "Doe"
+-- > }
 module Data.Aeson.Casing
     ( aesonDrop
     , aesonPrefix
--- src/Data/Aeson/Casing/Internal.hs
+++ src/Data/Aeson/Casing/Internal.hs
@@ -3,25 +3,50 @@ module Data.Aeson.Casing.Internal where
 import           Data.Aeson.Types
 import           Data.Char
 
+-- | Creates an Aeson options object that drops a specific number of characters
+-- from the front of a field name, then applies a casing function.
 aesonDrop :: Int -> (String -> String) -> Options
 aesonDrop n f = defaultOptions
         { fieldLabelModifier = f . drop n }
 
+-- | Creates an Aeson options object that drops the field name prefix from a
+-- field, then applies a casing function. We assume a convention of the prefix
+-- always being lower case, and the first letter of the actual field name being
+-- uppercase. This accommodated for field names in GHC 7.8 and below.
+--
+-- > data Person = Person
+-- >        { personFirstName :: Text
+-- >        , personLastName  :: Text
+-- >        } deriving (Generic)
+-- >
+-- > data Dog = Dog
+-- >        { dogFirstName :: Text
+-- >        } deriving (Generic)
+--
+-- In the above cases, dog and person are always dropped from the JSON field
+-- names.
 aesonPrefix :: (String -> String) -> Options
 aesonPrefix f = defaultOptions
         { fieldLabelModifier = f . dropFPrefix }
 
 ----
 
+-- | Snake casing, where the words are always lower case and separated by an
+-- underscore.
 snakeCase :: String -> String
 snakeCase = u . applyFirst toLower
     where u []                 = []
           u (x:xs) | isUpper x = '_' : toLower x : snakeCase xs
                    | otherwise = x : u xs
 
+-- | Camel casing, where the words are separated by the first letter of each
+-- word being a capitol. However, the first letter of the field is never a
+-- capitol.
 camelCase :: String -> String
 camelCase = applyFirst toLower
 
+-- | Pascal casing, where the words are separated by the first letter of each
+-- word being a capitol. The first letter of the field is always a capitol.
 pascalCase :: String -> String
 pascalCase = applyFirst toUpper
 
